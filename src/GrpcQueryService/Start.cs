b7e7a5a2e7b1615b01f3e308867a770f1f21f220
Rasmus Olsson <raol@L103066>
2018-03-06 21:57:51 +0100

    initial commit

---
+++ src/GrpcQueryService/Start.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Grpc.Core;
+
+namespace LuckQueryService
+{
+    public class Start
+    {
+        static void Main(string[] args)
+        {
+            int port = 54321;
+
+            var server = new Server
+            {
+                Services = { Queries.Contract.protos.GrpcQueryService.BindService(new LuckQueryService.Services.LuckQueryService()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("Server listening on port " + port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+
+        }
+    }
+}
---
+++ src/LuckClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Grpc.Core;
+
+using Queries.Contract.protos;
+
+namespace LuckQueryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var channel = new Channel("localhost:54321", ChannelCredentials.Insecure);
+
+            var client = new GrpcQueryService.GrpcQueryServiceClient(channel);
+            var input = "";
+
+            Console.WriteLine("Quit to Exit.");
+
+            Console.WriteLine("Type your name:");
+
+            while ((input = Console.ReadLine()) != "Quit")
+            {
+                var reply = client.GetLuck(new LuckRequest
+                {
+                    Name = input
+                });
+                Console.WriteLine("You luck today will be: " + reply.Procent);
+                Console.WriteLine(reply.Notification);
+            }
+          
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
